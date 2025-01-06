ebf63c20d8656ea75c29d49cce2240dff42f1a82
vbounmy <valentine.bounmy@gmail.com>
2025-01-06 14:50:47 +0100

    Fixing get_true_value function

--- lotka_volterra.py
+++ lotka_volterra.py
@@ -2,39 +2,59 @@ import matplotlib.pyplot as plt
 import numpy
 import pandas
 
-time = [0]
-lapin = [1]
-renard = [2]
+def get_true_value():
+    populations_lapins_renards = pandas.read_csv("populations_lapins_renards.csv", sep=',')
 
-alpha, beta, delta, gamma = 2/3, 4/3, 1, 1
+    true_value_lapin = populations_lapins_renards['lapin'].tolist()
+    true_value_renard = populations_lapins_renards['renard'].tolist()
 
-step = 0.001
+    return true_value_lapin, true_value_renard
 
-for _ in range(0, 100_000):
-    new_value_time = time[-1] + step
-    new_value_lapin = (lapin[-1] * (alpha - beta * renard[-1])) * step + lapin[-1]
-    new_value_renard = (renard[-1] * (delta * lapin[-1] - gamma)) * step + renard[-1]
+def get_pred_value():
+    time = [0]
+    lapin = [1]
+    renard = [2]
 
-    time.append(new_value_time)
-    lapin.append(new_value_lapin)
-    renard.append(new_value_renard)
+    alpha, beta, delta, gamma = 2/3, 4/3, 1, 1
 
-lapin = numpy.array(lapin)
-lapin *= 1000
+    step = 0.001
 
-renard = numpy.array(renard)
-renard *= 1000
+    for _ in range(0, 100_000):
+        new_value_time = time[-1] + step
+        new_value_lapin = (lapin[-1] * (alpha - beta * renard[-1])) * step + lapin[-1]
+        new_value_renard = (renard[-1] * (delta * lapin[-1] - gamma)) * step + renard[-1]
+
+        time.append(new_value_time)
+        lapin.append(new_value_lapin)
+        renard.append(new_value_renard)
+
+    lapin = numpy.array(lapin)
+    lapin *= 1000
+
+    renard = numpy.array(renard)
+    renard *= 1000
+
+    return lapin, renard
+
+def objectif(true_value, pred_value):
+    mse = numpy.square(numpy.subtract(true_value, pred_value)).mean()
+    return mse
 
 
+pred_lapin, pred_renard = get_pred_value()
+true_value_lapin, true_value_renard = get_true_value()
 
-populations_lapins_renards = pandas.read_csv("populations_lapins_renards.csv", sep=',')
+mse_lapin = objectif(true_value_lapin, pred_lapin[:len(true_value_lapin)])
+mse_renard = objectif(true_value_lapin, pred_lapin[:len(true_value_renard)])
 
+print(f"MSE lapin : {mse_lapin}")
+print(f"MSE renard : {mse_renard}")
 
-plt.figure(figsize=(15, 6))
-plt.plot(time, lapin, "b-", label='Lapins')
-plt.plot(time, renard, "r-", label='Renards')
-plt.xlabel('Temps (Mois)')
-plt.ylabel('Population')
-plt.title('Dynamique des populations Proie-Prédateur')
-plt.legend()
-plt.show()    
+# plt.figure(figsize=(15, 6))
+# plt.plot(time, lapin, "b-", label='Lapins')
+# plt.plot(time, renard, "r-", label='Renards')
+# plt.xlabel('Temps (Mois)')
+# plt.ylabel('Population')
+# plt.title('Dynamique des populations Proie-Prédateur')
+# plt.legend()
+# plt.show()    
